c08ca3896b32e2ea731d172788fad7a3fec16115
Michael Pavle <67883205+michalpv@users.noreply.github.com>
2020-09-26 23:08:03 -0400

    Added SHA256 hashing functionality

--- ProcHash.c
+++ ProcHash.c
@@ -1,14 +1,18 @@
 // Written by Michael Pavle
+/*
+Helpful resources:
+Hashing w/ CryptAPI - https://docs.microsoft.com/en-us/windows/win32/seccrypto/example-c-program--creating-an-md-5-hash-from-file-content
+*/
 #include <windows.h>
 #include <wincrypt.h>
 #include <psapi.h>
 #include <stdio.h>
-/*
-Necessary functions:
-EnumProcesses - https://docs.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-enumprocesses
-OpenProcess - https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-openprocess
-GetProcessInformation - https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getprocessinformation
-*/
+
+#pragma comment(lib, "Advapi32.lib")
+#pragma comment(lib, "Psapi.lib")
+
+#define BUFSIZE 1024
+#define SHA2LEN 32
 
 DWORD printErr(char *errInfo) {
 	DWORD err = GetLastError();
@@ -16,30 +20,79 @@ DWORD printErr(char *errInfo) {
 	return err;
 }
 
-char *getSha256Hash(char *filePath) { // Will return a NULL pointer if failed
+DWORD getSha256Hash(char *filePath, BYTE *fileHash) { // Will return 0 if failed, function returns size of file hash (In the case that I change the hashing algorithm), fileHash stores hash
 	// MSDN Example: https://docs.microsoft.com/en-us/windows/win32/seccrypto/example-c-program--creating-an-md-5-hash-from-file-content
 	HCRYPTPROV hProv = 0;
 	HCRYPTHASH hHash = 0;
-	// Get cryptographic service provider (CSP) context
-	//if(!CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT)) { // PROV_RSA_FULL DOES NOT support MD5 and SHA-2 hashing
-	//^ Error code 0x80090008 - https://stackoverflow.com/questions/25822745/using-sha2-512-calg-sha-512-on-windows-7-returns-invalid-algorithm-specified
 	if(!CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT)) { // PROV_RSA_AES supports MD5 and SHA-2 hashing
 		//^ https://docs.microsoft.com/en-us/windows/win32/api/wincrypt/nf-wincrypt-cryptacquirecontexta
 		printErr("Couldn't create cryptographic context");
-		return NULL;
+		return 0;
 	}
 	HCRYPTKEY hKey = 0; // Avoid differing type warnings for CryptCreateHash (don't just pass 0)
 	if (!CryptCreateHash(hProv, CALG_SHA_256, hKey, 0, &hHash)) { // SHA-256 > MD5
 		CryptReleaseContext(hProv, 0);
 		printErr("Couldn't create CSP hash object");
-		return NULL;
+		return 0;
 	}
 	// Get file handle and read content
 	HANDLE hFile = CreateFile(filePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL); // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea
 	if (hFile == INVALID_HANDLE_VALUE) {
 		printErr("Failed to open file handle");
-		return NULL;
+		CryptReleaseContext(hProv, 0);
+		CryptDestroyHash(hHash);
+		return 0;
 	}
+
+	// Read from file stream in chunks and call hashing function
+	BOOL result = FALSE;
+	BYTE fBuffer[BUFSIZE];
+	DWORD cbRead = 0;
+
+	do {
+		result = ReadFile(hFile, fBuffer, BUFSIZE, &cbRead, NULL);
+		if (cbRead == 0) {
+			break;
+		}
+
+		if (!CryptHashData(hHash, fBuffer, cbRead, 0)) {
+			printErr("CryptHashData failed");
+			CryptReleaseContext(hProv, 0);
+            CryptDestroyHash(hHash);
+            CloseHandle(hFile);
+			return 0;
+		}
+	} while(result);
+
+	CloseHandle(hFile); // No longer reading from file
+
+	if (!result) {
+		printErr("ReadFile failed");
+		CryptReleaseContext(hProv, 0);
+		CryptDestroyHash(hHash);
+		//CloseHandle(hFile);
+		return 0;
+	}
+
+	DWORD hashLen = SHA2LEN;
+	
+	if (!CryptGetHashParam(hHash, HP_HASHVAL, fileHash, &hashLen, 0)) {
+		printErr("CryptGetHashParam failed");
+	}
+	
+	// Print SHA-256 hash
+	CHAR rgbDigits[] = "0123456789abcdef";
+	printf("[+] SHA-256 hash of file %s is: ", filePath);
+	for (DWORD i = 0; i < hashLen; i++)
+	{
+		printf("%c%c", rgbDigits[fileHash[i] >> 4], rgbDigits[fileHash[i] & 0xf]);
+	}
+	printf("\n");
+		
+	CryptReleaseContext(hProv, 0);
+	CryptDestroyHash(hHash);
+	
+	return hashLen;
 }
 
 void procHandler(DWORD procID) {
@@ -47,14 +100,23 @@ void procHandler(DWORD procID) {
 	HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procID);
 	if (hProc != NULL) {
 		// Get process information and location of the executable
-		// https://stackoverflow.com/questions/4000877/how-can-i-get-the-current-instances-executable-file-name-from-native-win32-c
 		// GetModuleFileNameEx - Grabs file path of executable process - https://docs.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-getmodulefilenameexa
 		char filePath[256];
 		if (GetModuleFileNameEx(hProc, 0, filePath, sizeof(filePath)) == 0) {
 			printErr("Failed to get file path");
 		}
 		printf("[+] Process ID %d executable located at: %s\n", procID, filePath);
-		getSha256Hash(filePath); // Test
+		
+		BYTE fileHash[SHA2LEN]; // Hash length of SHA256 (32 byte array)
+		DWORD hashLen = getSha256Hash(filePath, fileHash); // Get SHA256 hash as BYTE array
+		if (hashLen == 0) {
+			printf("[-] Sha256Hash failed\n");
+			return;
+		}
+
+		// To Do:
+
+
 	}
 	CloseHandle(hProc);
 }
@@ -73,16 +135,3 @@ int main() {
 	getch();
 	return 0;
 }
-
-/*
-TODO LIST:
-Compute SHA256 hash/checksum of located executable and ask VirusTotal API for previous scan information
-	- https://stackoverflow.com/questions/13256446/compute-md5-hash-value-by-c-winapi
-	- https://docs.microsoft.com/en-us/windows/win32/seccrypto/example-c-program--creating-an-md-5-hash-from-file-content?redirectedfrom=MSDN
-	- If no file matching the hash was previously scanned, suspend process and ask user to scan file themselves on VirusTotal.
-If malicious process is found, suspend it? (ensure proper process access privileges have been set) TerminateProcess.
-^ Create separate thread for doing this so main loop continues
-^ https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-terminateprocess
-*/
-
-// VS Compiler Cmd line: cl OpenProcess.c /link Psapi.lib Advapi32.lib
