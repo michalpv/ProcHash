159e11af1c151a940ea219f351274d48979eb8b0
Michael Pavle <67883205+michalpv@users.noreply.github.com>
2020-10-14 18:20:29 -0400

    Populating fileInfoList, checking for duplicates

--- ProcHash.c
+++ ProcHash.c
@@ -1,8 +1,12 @@
-// Written by Michael Pavle
 /*
+Written by Michael Pavle
+
+Twitter: @_michalp
+Github: @michalpv
 Helpful resources:
 Hashing w/ CryptAPI - https://docs.microsoft.com/en-us/windows/win32/seccrypto/example-c-program--creating-an-md-5-hash-from-file-content
 */
+
 #include <windows.h>
 #include <wincrypt.h>
 #include <psapi.h>
@@ -58,6 +62,7 @@ DWORD getSha256Hash(char *filePath, char *fileHash) { // Will return 0 if failed
 	}
 	// Get file handle and read content
 	HANDLE hFile = CreateFile(filePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL); // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea
+	// ^ Opening files in C:\Windows\System32 directory throws system error code ERROR_FILE_NOT_FOUND (0x2)
 	if (hFile == INVALID_HANDLE_VALUE) {
 		printErr("Failed to open file handle");
 		CryptReleaseContext(hProv, 0);
@@ -150,20 +155,38 @@ int main() {
 	DWORD idProcList[1000]; // Expand buffer if necessary (Not easy to predict needed size)
 	DWORD cbNeeded;
 
-	struct fileInfo fileInfoList[1000];
+	struct fileInfo fileInfoList[1000]; // Same size as idProcList
 
 	// Populate idProcList and verify no error occurred
 	if (!EnumProcesses(idProcList, sizeof(idProcList), &cbNeeded)) {
 		return printErr("Failed to enumerate processes");
 	}
 	// TODO: Check that cbNeeded is equal to the size of idProcList; if so, recall EnumProcesses with larger array size
-	
+	int topIndex = 0;
 	for (int i = 0; i < (cbNeeded / sizeof(DWORD)); i++) { // cbNeeded will never exceed the size of idProcList, no bounds checking is required (EnumProcesses is safe)
 		struct fileInfo currentProc;
 		if (procHandler(idProcList[i], &currentProc)) {
-			printf("fileInfo struct - Path: %s\nHash: %s\nProcID: %d\n", currentProc.filePath, currentProc.hash, currentProc.procID); // For debugging
+			printf("fileInfo struct;\n\tPath: %s\n\tHash: %s\n\tProcID: %d\n", currentProc.filePath, currentProc.hash, currentProc.procID); // For debugging
+			// Enumerate each item in fileInfoList to make sure no duplicate hash exists
+			BOOL dupeExists = FALSE;
+			for (int index = 0; index < topIndex; index++) {
+				if (!strncmp(fileInfoList[index].hash, currentProc.hash, SHA2LEN*2)) { // Compare fileInfo.hash strings
+					dupeExists = TRUE;
+					break;
+				}
+			}
+			if (!dupeExists) { // If no duplicate hash exists in fileInfo struct array, add currentProc at fileInfoList[topIndex]
+				fileInfoList[topIndex] = currentProc;
+				topIndex++;
+			}
 		}
 		//CreateThread(NULL, 0, procHandler, (LPVOID) idProcList[i], 0, NULL);
+	}
+
+	// Enumerate each fileInfo struct in fileInfoList
+	printf("Filtered list:\n----------------------------\n");
+	for (int i = 0; i < topIndex; i++) {
+		printf("File path: %s\nHash: %s\nProcID: %d\n----------------------------\n", fileInfoList[i].filePath, fileInfoList[i].hash, fileInfoList[i].procID);
 	}
 	getch();
 	return 0;
